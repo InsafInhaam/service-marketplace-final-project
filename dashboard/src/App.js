1694a148722d0cc765f0097cf332ca60724c05c0
InsafInhaam <insafinhaam732@gmail.com>
2023-07-20 10:16:24 +0530

    dashboard

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Home from './screens/Home';
+import Login from './screens/Login';
 
 function App() {
   return (
     <Home/>
+    // <Login/>
   );
 }
 
--- dashboard/src/screens/Login.jsx
+++ dashboard/src/screens/Login.jsx
@@ -5,45 +5,42 @@ const Login = () => {
     <>
       <div className="login-form">
         <form action="/examples/actions/confirmation.php" method="post">
-          <div className="avatar">
-            <img src="/examples/images/avatar.png" alt="Avatar" />
-          </div>
-          <h2 className="text-center">Member Login</h2>
-          <div className="form-group">
-            <input
-              type="text"
-              className="form-control"
-              name="username"
-              placeholder="Username"
-              required="required"
-            />
-          </div>
-          <div className="form-group">
-            <input
-              type="password"
-              className="form-control"
-              name="password"
-              placeholder="Password"
-              required="required"
-            />
-          </div>
-          <div className="form-group">
-            <button type="submit" className="btn btn-primary btn-lg btn-block">
-              Sign in
-            </button>
-          </div>
-          <div className="clearfix">
-            <label className="pull-left checkbox-inline">
-              <input type="checkbox" /> Remember me
-            </label>
-            <a href="#" className="pull-right">
-              Forgot Password?
-            </a>
+          <div className="form-container">
+            <h2 className="text-center">Member Login</h2>
+            <div className="form-group">
+              <input
+                type="text"
+                className="form-control"
+                name="username"
+                placeholder="Username"
+                required="required"
+              />
+            </div> 
+            <div className="form-group">
+              <input
+                type="password"
+                className="form-control"
+                name="password"
+                placeholder="Password"
+                required="required"
+              />
+            </div>
+            <div className="form-group">
+              <button type="submit" className="btn btn-primary btn-lg btn-block">
+                Sign in
+              </button>
+            </div>
+            <div className="clearfix">
+              <label className="pull-left checkbox-inline">
+                <input type="checkbox" /> Remember me
+              </label>
+              <a href="#" className="pull-right">
+                Forgot Password?
+              </a>
+            </div>
           </div>
         </form>
-        <p className="text-center small">
-          Don't have an account? <a href="#">Sign up here!</a>
-        </p>
+        
       </div>
     </>
   );
